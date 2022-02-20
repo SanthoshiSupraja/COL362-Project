f576d56ee6b5c3a6eea354bb0dd9463c538484de
SanthoshiSupraja <62883037+SanthoshiSupraja@users.noreply.github.com>
2022-02-20 22:39:08 +0530

    Add files via upload

--- forms.py
+++ forms.py
@@ -4,11 +4,11 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
  class RegistrationForm(FlaskForm):
 
-   username -StringField(label='Username,validators-[DataRequired(), Length (min-3,max-20)])
+   username =StringField(label='Username,validators-[DataRequired(), Length (min-3,max-20)])
    email = StringField (label='Email',validators-[DataRequired(), Email()]) 7 password PasswordField (label="Password', validators-[DataRequired(), Length (min=6,max=16)])
    Bonding password=PasswordField(label='Confirm Password", validators-[DataRequired(), EqualTo('password"
    submit = SubmitField (label='Sign Up')
-  
+
 
  class LoginForm(FlaskForm):
 
---
+++ routes.py
@@ -0,0 +1,61 @@
+#from Project import app
+import os
+import psycopg2
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template,session
+
+app=Flask(__name__)
+
+def get_db_connection():
+    conn=psycopg2.connect(
+    host="localhost",
+    database="project",
+    user='postgres',
+    password='pass'
+    )
+    return conn
+
+@app.route("/")
+def home():
+    #conn=get_db_connection()
+    #cur=conn.cursor()
+    #cur.execute('SELECT * from users;')
+    #username = cur.fetchall()
+    #print(username)
+    return render_template("account.html",title='Account')
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+   conn=get_db_connection()
+   cur=conn.cursor
+   form=RegistrationForm()
+   if form.valididate_on_submit():
+      flas(f'Account created for {form.username.data}', category='success')
+      user_email=form.email.data
+      user_pwd=form.password.data
+      user_name=for.username.data
+      cur.execute('INSERT INTO users(username, emailid, password) VALUES ({}, {}, {});'.format(user_name,user_email,user_pwd))
+      return redirect(url_for('login'))
+   return render_template('register.html',title='Register',form='form')
+
+
+@app.route('/login', methods=['POST','GET'])
+def home():
+    conn=get_db_connection()
+    cur=conn.cursor
+    form-LoginForm()
+    if form.validate_on_submit():
+        user_email=form.email.data
+        user_pwd=form.password.data
+        cur.execute('SELECT password from users where emailid={};'.format(user_email))
+        pwd=cur.fetchone
+        if(pwd==None):
+           flash(f Login unsuccessful for (form.username.data)', category='danger')
+       else:
+         if pwd==user_pwd:
+            flash(f'Login successful for (form.username.data)', category="success")
+            return redirect (url_for('account'))
+         else:
+           flash(f Login unsuccessful for (form.username.data)', category='danger')
+    return render_template('login.html',title='Login', form='form')
+if __name__ == "__main__":
+    app.run(debug=True)
