100abb88cc8650387896b31cfbe82abc3eb9e70e
SanthoshiSupraja <62883037+SanthoshiSupraja@users.noreply.github.com>
2022-02-22 22:00:24 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,12 @@
 #from Project import app
 import os
 import psycopg2
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template,session,requ
-from forms import RegistrationForm,LoginForm
+from flask import Flask,flash, request, redirect, url_for, send_from_directory, render_template,session
+from .forms import RegistrationForm,LoginForm,HomepageForm
 
 app=Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisisfirstflaskapp'
 
 def get_db_connection():
     conn=psycopg2.connect(
@@ -15,32 +17,39 @@ def get_db_connection():
     )
     return conn
 
-@app.route("/")
-def home():
-    #conn=get_db_connection()
-    #cur=conn.cursor()
-    #cur.execute('SELECT * from users;')
-    #username = cur.fetchall()
-    #print(username)
-    return render_template("account.html",title='Account')
+
+'''
+    def home():
+        #conn=get_db_connection()
+        #cur=conn.cursor()
+        #cur.execute('SELECT * from users;')
+        #username = cur.fetchall()
+        #print(username)
+        return render_template("account.html",title='Account')
+'''
+@app.route('/')
+@app.route('/homepage', methods=['POST','GET'])
+def homepage():
+    form = HomepageForm()
+    return render_template('homepage.html',title='Homepage',form=form)
 
 @app.route('/register', methods=['POST','GET'])
 def register():
-   conn=get_db_connection()
-   cur=conn.cursor
-   form=RegistrationForm()
-   if form.valididate_on_submit():
-      flash(f'Account created for {form.username.data}', category='success')
-      user_email=form.email.data
-      user_pwd=form.password.data
-      user_name=form.username.data
-      cur.execute('INSERT INTO users(username, emailid, password) VALUES ({}, {}, {});'.format(user_name,user_email,user_pwd))
-      return redirect(url_for('login'))
-   return render_template('register.html',title='Register',form='form')
-
+    conn=get_db_connection()
+    cur=conn.cursor()
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user_email=form.email.data
+        user_pwd=form.password.data
+        sql="INSERT INTO users(username, emailid, password) VALUES ('{}', '{}', '{}');".format(form.username.data,user_email,user_pwd)
+        cur.execute(sql)
+        conn.commit()
+        flash(f'Account created for {form.username.data}', category='success')
+        return redirect(url_for('homepage'))
+    return render_template('register.html',title='Register',form=form)
 
 @app.route('/login', methods=['POST','GET'])
-def home():
+def homeq():
     conn=get_db_connection()
     cur=conn.cursor
     form=LoginForm()
